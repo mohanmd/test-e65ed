379e11fdd28f7fcb4756170dcb84ba7cf8a3aa0f
Stackbit Code Editor <projects@stackbit.com>
2021-07-14 09:04:21 +0000

    SectionGrid.js: changed

--- src/components/SectionGrid.js
+++ src/components/SectionGrid.js
@@ -8,7 +8,8 @@ export default class SectionGrid extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         return (
-            <section id={_.get(section, 'section_id', null)} className={classNames('block', 'block-grid', 'outer', {'has-header': _.get(section, 'title', null) || _.get(section, 'subtitle', null)})}>
+            <se
+            ction id={_.get(section, 'section_id', null)} className={classNames('block', 'block-grid', 'outer', {'has-header': _.get(section, 'title', null) || _.get(section, 'subtitle', null)})}>
               <div className="inner">
                 {(_.get(section, 'title', null) || _.get(section, 'subtitle', null)) && (
                 <div className="block-header inner-sm">
