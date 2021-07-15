c0f3bf220eac79bb6c005490b7e1ddd8526974ca
Stackbit Code Editor <projects@stackbit.com>
2021-07-15 05:15:18 +0000

    FooterTop.js: changed

--- src/components/FooterTop.js
+++ src/components/FooterTop.js
@@ -24,10 +24,7 @@ export default class FooterTop extends React.Component {
                 <div className="grid-item text-center">
                     {_.get(this.props, 'pageContext.site.siteMetadata.footertop.content', null) && (
                     <span className="copyright">{htmlToReact(_.get(this.props, 'pageContext.site.siteMetadata.footertop.content', null))}</span>
-                    )}
-                    {_.map(_.get(this.props, 'pageContext.site.siteMetadata.footer.links', null), (action, action_idx) => (
-                      <ActionLink key={action_idx} {...this.props} action={action} />
-                    ))}
+                    )} 
                   </div>
 
                   {_.get(this.props, 'pageContext.site.siteMetadata.footer.has_social', null) && (
