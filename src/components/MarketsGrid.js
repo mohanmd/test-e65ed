56328e164189343a1bff0e09a294248a039452f6
Stackbit Code Editor <projects@stackbit.com>
2021-07-17 11:43:42 +0000

    MarketsGrid.js: changed

--- src/components/MarketsGrid.js
+++ src/components/MarketsGrid.js
@@ -4,7 +4,7 @@ import _ from 'lodash';
 import {classNames, htmlToReact, withPrefix, Link, markdownify} from '../utils';
 import CtaButtons from './CtaButtons';
 
-export default class AboutGrid extends React.Component {
+export default class MarketsGrid extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         console.log(section)
