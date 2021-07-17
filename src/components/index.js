1ae8cd8eb9490576862a7f8efa0415ee81e17cbe
Stackbit Code Editor <projects@stackbit.com>
2021-07-17 12:47:15 +0000

    index.js: changed

--- src/components/index.js
+++ src/components/index.js
@@ -15,6 +15,7 @@ import Submenu from './Submenu';
 import Layout from './Layout';
 import AboutGrid from './AboutGrid';
 import MarketsGrid from './MarketsGrid';
+import MarketsGrid from './MarketsGrid';
 CapabilityGrid
 export {
     ActionLink,
