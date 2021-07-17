a4b0e19c41d8d0a3722ea791ca6a1bd46d3300fc
Stackbit Code Editor <projects@stackbit.com>
2021-07-17 11:46:36 +0000

    MarketsGrid.js: changed

--- src/components/MarketsGrid.js
+++ src/components/MarketsGrid.js
@@ -8,6 +8,7 @@ export default class MarketsGrid extends React.Component {
     render() {
         let section = _.get(this.props, 'section', null);
         console.log(section)
+        alert('sdfsdf')
         return (
             <section id={_.get(section, 'section_id', null)} className={classNames('block', 'block-grid', 'outer', {'has-header': _.get(section, 'title', null) || _.get(section, 'subtitle', null)})}>
               <div className="inner">fdgfg
