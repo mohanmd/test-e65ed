90450e9e11c3a871527dc97db5866a757d918dbc
Stackbit Code Editor <projects@stackbit.com>
2021-07-19 07:03:16 +0000

    StartCareer.js: changed

--- src/components/StartCareer.js
+++ src/components/StartCareer.js
@@ -11,7 +11,7 @@ export default class StartCareer extends React.Component {
         console.log('sdfdsf')
         console.log(section)
         return (
-            <section id={_.get(section, 'section_id', null)} className= {classNames('block', 'start-career', 'block-grid', 'outer',{_.get(section, 'section_id', null)}, {'has-header': _.get(section, 'title', null) || _.get(section, 'subtitle', null)})}>
+            <section id={_.get(section, 'section_id', null)} className= {classNames('block', 'start-career', 'block-grid', 'outer', {'has-header': _.get(section, 'title', null) || _.get(section, 'subtitle', null)})}>
               {/* <div className="inner"> */}
                 {(_.get(section, 'title', null) || _.get(section, 'subtitle', null)) && (
                 <div className="block-header inner-sm">
