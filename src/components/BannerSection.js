9ea93b357673397069e5c5647a7064eccdc44189
Stackbit Code Editor <projects@stackbit.com>
2021-07-19 05:55:41 +0000

    BannerSection.js: changed

--- src/components/BannerSection.js
+++ src/components/BannerSection.js
@@ -32,6 +32,28 @@ export default class BannerSection extends React.Component {
                           <img src={withPrefix(_.get(item, 'image', null))} alt={_.get(item, 'image_alt', null)} />
                         </div>
                         )}
+
+                        <div class="slider__inner">
+                            {_.get(item, 'title', null) && (
+                        <h3 className="grid-item-title line-left">
+                          {_.get(item, 'title_url', null) ? (
+                          <Link to={withPrefix(_.get(item, 'title_url', null))}>{_.get(item, 'title', null)}</Link>
+                          ) : 
+                          _.get(item, 'title', null)
+                          }
+                        </h3>
+                        )}
+                        {_.get(item, 'content', null) && (
+                        <div className="grid-item-content">
+                          {markdownify(_.get(item, 'content', null))}
+                        </div>
+                        )}
+                        {_.get(item, 'actions', null) && (
+                        <div className="grid-item-buttons">
+                          <CtaButtons {...this.props} actions={_.get(item, 'actions', null)} />
+                        </div>
+                        )}
+                        </div>
                         
                       </div>
                     </div>
