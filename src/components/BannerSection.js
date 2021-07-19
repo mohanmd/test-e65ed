908d58f28a0ad96e5bffc75fdb36365b42a8d237
Stackbit Code Editor <projects@stackbit.com>
2021-07-19 05:53:50 +0000

    BannerSection.js: changed

--- src/components/BannerSection.js
+++ src/components/BannerSection.js
@@ -10,7 +10,7 @@ export default class BannerSection extends React.Component {
         console.log(section)
         return (
             <section id={_.get(section, 'section_id', null)} className={classNames('block', 'block-grid', 'outer', {'has-header': _.get(section, 'title', null) || _.get(section, 'subtitle', null)})}>
-              <div className="inner">
+              {/* <div className="inner"> */}
                 {(_.get(section, 'title', null) || _.get(section, 'subtitle', null)) && (
                 <div className="block-header inner-sm">
                   {_.get(section, 'title', null) && (
@@ -57,7 +57,7 @@ export default class BannerSection extends React.Component {
                   </div>
                 </div>
                 )}
-              </div>
+              {/* </div> */}
             </section>
         );
     }
